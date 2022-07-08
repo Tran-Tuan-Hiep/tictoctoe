f5d5fac0a080075d2916ae81f249b58cfc01e947
Tran-Tuan-Hiep <trantuanhiep07022001@gmail.com>
2022-07-09 00:12:45 +0700

    add file ttt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTokToe from './TikTocToe';
+
+function App() {
+  return (
+     <div>
+      <TicTokToe/>
+     </div>
+  );
+}
+
+export default App;
